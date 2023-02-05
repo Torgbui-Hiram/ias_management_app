003ad39d6892a944b146743d7b6c56c1eaa42ded
Torgbui-Hiram <hiramkumado@gmail.com>
2023-02-05 10:12:54 +0000

    added activate link to urls

--- members/urls.py
+++ members/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('authenticate', views.login_user, name='login'),
     path('register', views.signup, name='signup'),
     path('signout', views.logout_user, name='logout'),
+    path('activate/<uidb64>/<token>/', views.activate, name='activate'),
 ]
